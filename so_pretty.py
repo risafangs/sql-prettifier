3c86e4425049834deede7f6187fe24dcf42f8495
Lisa Fang <lisa.fang@brooklyndata.co>
2023-03-23 13:31:31 -0500

    use text area

--- so_pretty.py
+++ so_pretty.py
@@ -9,7 +9,5 @@ streamlit.header('Prettier SQL for All!')
 streamlit.text('Paste your ugly all-caps SQL into the box below and experience magical transformation.')
 
 # form
-input_form = streamlit.form("ugly_sql")
+sql_input = streamlit.text_area(sql_input, placeholder="Input query text here", label_visibility=hidden)
 
-# Now add a submit button to the form:
-input_form.form_submit_button("submit")
