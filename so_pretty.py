68d09aac07dc6b23d8b3f9673d0a245187dbe021
Lisa Fang <lisa.fang@brooklyndata.co>
2023-03-23 13:22:41 -0500

    initialize streamlit stuff

---
+++ so_pretty.py
@@ -0,0 +1,17 @@
+# imports
+
+import streamlit
+
+
+# visual start
+
+streamlit.header('Prettier SQL for All!')
+streamlit.text('Paste your ugly all-caps SQL into the box below and experience magical transformation.')
+
+# form
+input_form = streamlit.form("ugly_sql")
+form.slider("Inside the form")
+streamlit.slider("Outside the form")
+
+# Now add a submit button to the form:
+form.form_submit_button("submit")
