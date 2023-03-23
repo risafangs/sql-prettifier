90faa10c20b5fec4a3348c8da7e078fcb880c36f
Lisa Fang <lisa.fang@brooklyndata.co>
2023-03-23 13:32:38 -0500

    remove label

--- so_pretty.py
+++ so_pretty.py
@@ -9,5 +9,5 @@ streamlit.header('Prettier SQL for All!')
 streamlit.text('Paste your ugly all-caps SQL into the box below and experience magical transformation.')
 
 # form
-sql_input = streamlit.text_area(sql_input, placeholder="Input query text here", label_visibility=hidden)
+sql_input = streamlit.text_area(placeholder="Input query text here", label_visibility=hidden)
 
