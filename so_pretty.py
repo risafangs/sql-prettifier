0cf0ae8dbc7fa7a0143c12b611a5dab64604e295
Lisa Fang <lisa.fang@brooklyndata.co>
2023-03-23 13:38:14 -0500

    try adding output box

--- so_pretty.py
+++ so_pretty.py
@@ -6,10 +6,17 @@ import streamlit
 # visual start
 
 streamlit.header('Prettier SQL for All!')
-streamlit.text('Paste your ugly all-caps SQL into the box below and experience magical transformation.')
 
-# form
-label_text = "You know what to do"
+# form stuff
+
+label_text = "Paste your SQL into the box below and experience magical transformation."
 sql_input = streamlit.text_area(label=label_text)
 
-print(sql_input.lower())
+# stop the screaming
+output = sql_input.lower()
+
+
+
+# display output in a code block
+streamlit.text('✨🌟✨🌟✨')
+streamlit.code(output, language='sql')
